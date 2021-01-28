9e463166e63db6271a082acce5e63b7c18e6e44e
Daniel Aird <djrd467@gmail.com>
2021-01-27 19:50:06 -0500

    Assembly code has been written

--- Core/Src/part1.s
+++ Core/Src/part1.s
@@ -4,5 +4,45 @@
 .global kalman
 
 kalman:
-	mov r0, #1
+	//r0 -> pointer to the struct, also is the pointer to q since that is the first float in the struct
+	//s0 -> measurement float
+	//s1 -> q
+	//s2 -> r
+	//s3 -> x
+	//s4 -> p
+	//s5 -> k
+	//s6 -> used for intermediate in math
+
+	//CALLEE-SAVE CONVENTION
+	vpush {s1-s6, lr}
+
+	vldr s1, [r0] //q
+	vldr s2, [r0, #4] //r
+	vldr s3, [r0, #8] //x
+	vldr s4, [r0, #12] //p
+	vldr s5, [r0, #16] //k
+
+	//p = p + q ---> s4 = s4 + s1
+	vadd.f32 s4, s4, s1
+
+	//k = p/(p+r)
+	vadd.f32 s6, s4, s2
+	vdiv.f32 s5, s4, s6
+	vstr s5, [r0, #16] //Store new k
+
+	//x = x + k(measure - x)
+	vsub.f32 s6, s0, s3
+	vmul.f32 s6, s5, s6
+	vadd.f32 s3, s3, s6
+	vstr s3, [r0, #8] //Store new x
+
+	//p = (1-k)p
+	vmov s6, #1
+	vsub.f32 s6, s6, s5
+	vmul.f32 s4, s6, s4
+	vstr s4, [r0, #12] //Store new p
+
+	//CALLEE-SAVE CONVENTION
+	vpop {s1-s6, lr}
+
 	bx lr
